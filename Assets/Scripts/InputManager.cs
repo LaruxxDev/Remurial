258161051a19ad66443fe06efca35b83b45a0ca1
smorcor744 <148891340+smorcor744@users.noreply.github.com>
2026-02-19 10:25:04 +0100

    camara

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -1,30 +1,49 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+
 public class InputManager : MonoBehaviour
 {
-    float horizontalMovement;
-    float verticalMovement;
-    AnimatorManager animatorManager;
+    [SerializeField] private InputActionReference moveAction; 
+    private Rigidbody rb;
+    public float moveSpeed = 5f;
+    public float rotationSpeed = 10f;
+
+    public Vector2 movementInput { get; private set; }
+
+    private AnimatorManager animatorManager;
+    private Transform cameraTransform;
 
     private void Awake()
     {
         animatorManager = GetComponent<AnimatorManager>();
+        cameraTransform = Camera.main.transform;
     }
 
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();    
+    }
+    
     private void Update()
     {
 
-        if (Keyboard.current != null)
+        movementInput = moveAction.action.ReadValue<Vector2>();
+
+        animatorManager.HandleAnimatorValues(movementInput.x, movementInput.y);
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 moveDirection = new Vector3(movementInput.x, 0, movementInput.y);
+        moveDirection = cameraTransform.TransformDirection(moveDirection);
+        moveDirection.y = 0;
+
+        rb.linearVelocity = new Vector3(moveDirection.x * moveSpeed, rb.linearVelocity.y, moveDirection.z * moveSpeed);
+
+        if (moveDirection != Vector3.zero)
         {
-            if (Keyboard.current.aKey.isPressed || Keyboard.current.leftArrowKey.isPressed) horizontalMovement = -1;
-            else if (Keyboard.current.dKey.isPressed || Keyboard.current.rightArrowKey.isPressed) horizontalMovement = 1;
-            else horizontalMovement = 0;
-
-            if (Keyboard.current.wKey.isPressed || Keyboard.current.upArrowKey.isPressed) verticalMovement = 1;
-            else if (Keyboard.current.sKey.isPressed || Keyboard.current.downArrowKey.isPressed) verticalMovement = -1;
-            else verticalMovement = 0;
+            Quaternion targetRotation = Quaternion.LookRotation(moveDirection);
+            rb.MoveRotation(Quaternion.Slerp(transform.rotation, targetRotation, Time.fixedDeltaTime * rotationSpeed));
         }
-
-        animatorManager.HandleAnimatorValues(horizontalMovement, verticalMovement);
-    }   
+    }
 }
